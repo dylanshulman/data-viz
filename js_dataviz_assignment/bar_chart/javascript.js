9cdea0231c64095f947a219498f0087a4c8d431d
dylanshulman <shulmandylan@gmail.com>
2024-04-15 19:43:03 -0400

    Update

---
+++ js_dataviz_assignment/bar_chart/javascript.js
@@ -0,0 +1,32 @@
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Don\'t Start Now', 'One Kiss by Calvin Harris', 'Levitating (feat. DaBaby)', 'Cold Heart - PNAU Remix', 'No Lie by Sean Paul'],
+        datasets: [{
+            label: 'Streams',
+            data: [2303033973, 1897517891, 1802514301, 1605224506, 956865266],
+            backgroundColor: [
+                '#8c1ddb'
+            ]
+        },
+    ]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+                position: 'top',
+              },
+            title: {
+                display: true,
+                text: 'Dua Lipa\'s Top Streamed Songs in 2023',
+                font: {
+                    size: 18
+                }
+            },
+            subtitle: {
+                display: true,
+                text: 'Including her features, Dua Lipa\'s top five tracks collectively earned over 8.5 billion streams. \"Don\'t Start Now\" 
