1f52a5f9d58269399f4e16c50b83a0a18c0d2461
dylanshulman <shulmandylan@gmail.com>
2024-04-15 23:28:59 -0400

    Update

--- js_dataviz_assignment/bubble_chart/javascript.js
+++ js_dataviz_assignment/bubble_chart/javascript.js
@@ -7,21 +7,21 @@
               datasets: [{
                 label: 'Dua Lipa\'s Top 5 Streamed Songs',
                 data: [
-            {x: 27119, y: 532, r: 2303033973 / 100000000},
-            {x: 27705, y: 537, r: 1897517891 / 100000000},
-            {x: 15894, y: 198, r: 1802514301 / 100000000},
-            {x: 21097, y: 384, r: 1605224506 / 100000000},
-            {x: 7370, y: 92, r: 956865266 / 100000000}
+            {x: 27119, y: 532, r: 2303033973 / 100000000, label: 'Don\'t Start Now'},
+            {x: 27705, y: 537, r: 1897517891 / 100000000, label: 'One Kiss (with Dua Lipa) by Calvin Harris'},
+            {x: 15894, y: 198, r: 1802514301 / 100000000, label: 'Levitating (feat. DaBaby)'},
+            {x: 21097, y: 384, r: 1605224506 / 100000000, label: 'Cold Heart - PNAU Remix'},
+            {x: 7370, y: 92, r: 956865266 / 100000000, label: 'No Lie by Sean Paul'}
           ]
               },
               {
                 label: 'Billie Eilish\'s Top 5 Streamed Songs',
                 data: [
-            {x: 15032, y: 221, r: 2355719893 / 100000000},
-            {x: 8476, y: 138, r: 1056760045 / 100000000},
-            {x: 4204, y: 39, r: 777765388 / 100000000},
-            {x: 1959, y: 19, r: 412795151 / 100000000},
-            {x: 3009, y: 68, r: 338564981 / 100000000}
+            {x: 15032, y: 221, r: 2355719893 / 100000000, label: 'lovely - Bonus Track'},
+            {x: 8476, y: 138, r: 1056760045 / 100000000, label: 'Happier Than Ever'},
+            {x: 4204, y: 39, r: 777765388 / 100000000, label: 'Bored'},
+            {x: 1959, y: 19, r: 412795151 / 100000000, label: 'Happier Than Ever - Edit'},
+            {x: 3009, y: 68, r: 338564981 / 100000000, label: 'TV'}
           ]
               }]
             },
@@ -41,16 +41,29 @@
                   text: '# of Spotify playlists with track'
                 }
             }
-        },
+        },       
               responsive: true,
               plugins: {
+                tooltip: {
+                  enabled: true,
+                  callbacks: {
+                    label: function(context) {
+                      const label = context.dataset.label || '';
+                      const dataPoint = context.raw;
+                      const x = dataPoint.x;
+                      const y = dataPoint.y;
+                      const r = dataPoint.r;
+                      return `${label}: (X: ${x}, Y: ${y}, R: ${r})`;
+                }
+            }
+        },
               title: {
                 display: true,
-                text: 'Audio features of Dua Lipa\'s latest singles'
+                text: 'Top Streamed Songs by Dua Lipa and Billie Eilish'
               },
             subtitle: {
                 display: true,
-                text: 'With \"Optimism\" dropping at the beginning of May, Dua Lipa\'s last three singles give us a hint at what the new album will sound like.'
+                text: 'Both pop superstars will release albums in May. Will Eilish\'s new project put her ahead of Lipa? Bubble size represents track streams.'
             },
               elements: {
                 line: {
